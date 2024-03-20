9d01653a7e6d6efc1c21f4f06135cdfa3087d139
YoussefAshraf22 <useefashraf22@gmail.com>
2024-03-20 18:37:03 +0200

    Profile shared

---
+++ lib/shared/profile_menu.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({super.key, required this.title, required this.icon, required this.onPress, this.textColor});
+  final  String title;
+  final IconData icon;
+  final VoidCallback onPress;
+  final bool endTccon = true;
+  final Color? textColor;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Colors.grey.withOpacity(0.1),
+        ),
+        child:  Icon(
+          icon,
+          color: Colors.blue,
+        ),
+      ),
+      title:  Text(title,style:TextStyle(color: textColor),),
+      trailing: endTccon?
+      Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Colors.grey.withOpacity(0.1),
+        ),
+        child: IconButton(
+            onPressed: onPress,
+            icon: const Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: Colors.grey,
+              size: 15,
+            )),
+      ):null
+    );
+  }
+}
