60a8ca545327b382ed41cbec2a2cf14e5fed5bf5
YoussefAshraf22 <useefashraf22@gmail.com>
2024-04-06 23:29:28 +0200

    edit profile

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:zootopia/pages/profile/profile_page.dart';
+import 'package:image_picker/image_picker.dart';
 
-class EditProfile extends StatelessWidget {
+import '../../shared/show_Image_picker.dart';
+
+class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
+class _EditProfileState extends State<EditProfile> {
+  File? selectedImage;
+  File? selectedCover;
 
   @override
   Widget build(BuildContext context) {
@@ -23,117 +35,202 @@ class EditProfile extends StatelessWidget {
           ],
         ),
         body: SingleChildScrollView(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-              Center(
-                child: Stack(alignment: Alignment.bottomRight, children: [
-                  const CircleAvatar(
-                    radius: 140,
-                    child: Image(
-                      image: AssetImage(
-                        'assets/images/logo2.jpg',
-                      ),
-                    ),
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Colors.grey,
-                    radius: 20,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.camera_alt),
-                    ),
-                  )
-                ]),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Form(
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        onChanged: (val) {
-                          val = fullName;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            label: const Text('Full Name'),
-                            prefixIcon: const Icon(Icons.person)),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      TextFormField(
-                        onChanged: (val) {
-                          val = email;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            label: const Text('Email'),
-                            prefixIcon: const Icon(Icons.email)),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      TextFormField(
-                        onChanged: (val) {
-                          val = password;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            label: const Text('Password'),
-                            prefixIcon: const Icon(Icons.lock)),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      TextFormField(
-                        onChanged: (val) {
-                          val = phone!;
-                        },
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            label: const Text('Phone Number'),
-                            prefixIcon: const Icon(Icons.phone_iphone_rounded)),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              SizedBox(
+            child: Column(children: [
+          Stack(children: [
+            SizedBox(
+                height: 180,
                 width: double.infinity,
-                height: 40,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        side: BorderSide.none,
-                        shape: const StadiumBorder()),
+                child: selectedCover != null
+                    ? Image.file(selectedCover!)
+                    : Image.asset('assets/images/profile_cover.webp')),
+            Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 18,
+                  child: IconButton(
                     onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const ProfilePage(),
-                          ));
+                      const ShowImagePicker().showImagePicker(
+                          context: context,
+                          imageCamera: pickCoverCamera,
+                          imageGallery: pickCoverGallery);
                     },
-                    child: const Text(
-                      'Save Edites',
-                      style: TextStyle(color: Colors.white),
-                    )),
+                    icon: const Icon(
+                      Icons.edit,
+                      size: 18,
+                    ),
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.only(top: 40, bottom: 20),
+              child: selectedImage != null
+                  ? Center(
+                      child: SizedBox(
+                        height: 180,
+                        width: 180,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: Image.file(selectedImage!),
+                        ),
+                      ),
+                    )
+                  : Center(
+                      child: SizedBox(
+                        height: 180,
+                        width: 180,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(100),
+                          child: const Image(
+                            image: AssetImage(
+                              'assets/images/logo2.jpg',
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+            ),
+            Positioned(
+                bottom: 8,
+                right: 120,
+                child: CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 20,
+                  child: IconButton(
+                    onPressed: () {
+                      const ShowImagePicker().showImagePicker(
+                          context: context,
+                          imageCamera: pickImageCamera,
+                          imageGallery: pickImageGallery);
+                    },
+                    icon: const Icon(Icons.edit),
+                  ),
+                ))
+          ]),
+          const SizedBox(
+            height: 25,
+          ),
+          Form(
+            child: Padding(
+              padding: const EdgeInsets.all(14),
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.name,
+                    onChanged: (val) {
+                      val = fullName;
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        label: const Text('Full Name'),
+                        prefixIcon: const Icon(Icons.person)),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (val) {
+                      val = email;
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        label: const Text('Email'),
+                        prefixIcon: const Icon(Icons.email)),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    onChanged: (val) {
+                      val = password;
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        label: const Text('Password'),
+                        prefixIcon: const Icon(Icons.lock)),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    onChanged: (val) {
+                      val = phone!;
+                    },
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        label: const Text('Phone Number'),
+                        prefixIcon: const Icon(Icons.phone_iphone_rounded)),
+                  ),
+                ],
               ),
-            ])),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: SizedBox(
+              width: double.infinity,
+              height: 40,
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    side: BorderSide.none,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'Save edits',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+          ),
+        ])),
       ),
     );
+  }
+
+  Future pickImageGallery() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnImage == null) return;
+    setState(() {
+      selectedImage = File(returnImage.path);
+    });
+  }
+
+  Future pickImageCamera() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnImage == null) return;
+    setState(() {
+      selectedImage = File(returnImage.path);
+    });
+  }
+
+  Future pickCoverGallery() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (returnImage == null) return;
+    setState(() {
+      selectedCover = File(returnImage.path);
+    });
+  }
+
+  Future pickCoverCamera() async {
+    final returnImage =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (returnImage == null) return;
+    setState(() {
+      selectedCover = File(returnImage.path);
+    });
   }
 }
